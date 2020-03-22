35ba47dbb470c7e259af1805252431e02bc9c036
hsc <hsc@fmg-guides.com>
2020-03-22 22:55:06 +0100

    added (setup) broker command & controller

--- Controller/JobController.php
+++ Controller/JobController.php
@@ -28,6 +28,8 @@ class JobController extends AbstractController
      */
     public function list(Request $request)
     {
+        $request = new \GuzzleHttp\Psr7\Request($request->getMethod(), $request->getUri(), $request->headers->all(), $request->getContent(), $request->getProtocolVersion());
+
         return $this->createResponse($this->controller->list($request->getQueryString(), $request->getUri()));
     }
 
--- DependencyInjection/AbcJobServerExtension.php
+++ DependencyInjection/AbcJobServerExtension.php
@@ -2,6 +2,7 @@
 
 namespace Abc\JobServerBundle\DependencyInjection;
 
+use Abc\Job\Broker\Broker;
 use Abc\Job\Broker\Config;
 use Abc\Job\Interop\DriverFactory;
 use Abc\Job\Interop\DriverInterface;
@@ -24,25 +25,42 @@ class AbcJobServerExtension extends Extension implements PrependExtensionInterfa
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config/services'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config/services'));
 
         $loader->load('services.yml');
 
         $diUtils = DiUtils::create();
 
         $driverFactoryId = $diUtils->format('driver_factory');
-        $container->register($driverFactoryId, DriverFactory::class)->addArgument(new Reference($diUtils->format('managed_route_registry')))->addArgument(new Reference('logger'));
+        $container->register($driverFactoryId, DriverFactory::class)->addArgument(
+            new Reference($diUtils->format('managed_route_registry'))
+        )->addArgument(new Reference('logger'));
 
         $driverId = $diUtils->format('driver');
-        $container->register($driverId, DriverInterface::class)->setFactory([
-            new Reference($driverFactoryId),
-            'create',
-        ])->addArgument(new Reference(sprintf('enqueue.transport.%s.context', $config['transport'])));
+        $container->register($driverId, DriverInterface::class)->setFactory(
+            [
+                new Reference($driverFactoryId),
+                'create',
+            ]
+        )->addArgument(new Reference(sprintf('enqueue.transport.%s.context', $config['transport'])));
+
+        // broker
+        $broker = $container->register($diUtils->format('broker.' . $config['transport']), Broker::class)
+            ->setArguments(
+                [
+                    $config['transport'],
+                    new Reference($driverId),
+                    new Reference($diUtils->format('route_registry'))
+                ]
+            );
+
+        $brokerRegistry = $container->getDefinition($diUtils->format('broker_registry'));
+        $brokerRegistry->addMethodCall('add', [$config['transport'], $broker]);
 
         // scheduler
         if (false == empty($config['cronjob']['enabled'])) {
             $bundles = $container->getParameter('kernel.bundles');
-            if (! isset($bundles['AbcSchedulerBundle'])) {
+            if (!isset($bundles['AbcSchedulerBundle'])) {
                 throw new \LogicException('The "aboutcoders/scheduler-bundle" package has to be installed.');
             }
 
@@ -67,26 +85,29 @@ class AbcJobServerExtension extends Extension implements PrependExtensionInterfa
     {
         $bundles = $container->getParameter('kernel.bundles');
 
-        if (! isset($bundles['DoctrineBundle'])) {
+        if (!isset($bundles['DoctrineBundle'])) {
             throw new \LogicException('The "doctrine/doctrine-bundle" package has to be installed.');
         }
 
         foreach ($container->getExtensionConfig('doctrine') as $config) {
-            if (! empty($config['dbal'])) {
+            if (!empty($config['dbal'])) {
                 $rc = new \ReflectionClass(Job::class);
                 $rootDir = dirname($rc->getFileName());
-                $container->prependExtensionConfig('doctrine', [
-                    'orm' => [
-                        'mappings' => [
-                            'abc_job' => [
-                                'is_bundle' => false,
-                                'type' => 'xml',
-                                'dir' => $rootDir.'/Doctrine/mapping',
-                                'prefix' => 'Abc\Job\Model',
+                $container->prependExtensionConfig(
+                    'doctrine',
+                    [
+                        'orm' => [
+                            'mappings' => [
+                                'abc_job' => [
+                                    'is_bundle' => false,
+                                    'type' => 'xml',
+                                    'dir' => $rootDir . '/Doctrine/mapping',
+                                    'prefix' => 'Abc\Job\Model',
+                                ],
                             ],
                         ],
-                    ],
-                ]);
+                    ]
+                );
                 break;
             }
         }
